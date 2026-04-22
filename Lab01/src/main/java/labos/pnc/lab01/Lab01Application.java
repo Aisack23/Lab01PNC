c90b58baac2b229d6d241ebd0b38da2408284004
@cesar.isaac.tovar22 <cesar.isaac.tovar22@gmail.com>
2026-04-22 12:58:22 -0600

    :rocket: feat(runner): implement STARSReportRunner for console output

--- Lab01/src/main/java/labos/pnc/lab01/Lab01Application.java
+++ Lab01/src/main/java/labos/pnc/lab01/Lab01Application.java
@@ -1,10 +1,7 @@
 package labos.pnc.lab01;
 
-import labos.pnc.lab01.service.BowService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Lab01Application {
@@ -13,14 +10,6 @@ public class Lab01Application {
         SpringApplication.run(Lab01Application.class, args);
     }
 
-    @Bean
-    public CommandLineRunner run(BowService productService) {
-        return args -> {
-            System.out.println("=== INICIANDO APP ===");
-            productService.findAll().forEach(p ->
-                    System.out.println(p.getName() + " - $" + p.getPrice())
-            );
-        };
-    }
+
 
 }
